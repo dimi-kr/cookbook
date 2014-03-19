310c30325f2b5105b0112c469374261ace71db1a
Dimetrio <dimetrio@flysoft.ru>
2014-03-19 17:35:49 +0400

    new test module

---
+++ basic/metadata.rb
@@ -0,0 +1,8 @@
+name             "basic"
+maintainer       "Dimetrio"
+maintainer_email "kiddo.desu@gmail.com"
+license          "All rights reserved"
+description      "Basic system config"
+version          "0.1.0"
+depends          "yum"
+
