1acdba3dcd65d28d53074e3868e190713100b9dc
Dimetrio <dimetrio@flysoft.ru>
2014-03-19 17:40:29 +0400

    fix

--- basic/metadata.rb
+++ basic/metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "kiddo.desu@gmail.com"
 license          "All rights reserved"
 description      "Basic system config"
 version          "0.1.0"
-depends          "yum"
+depends          "yum, nginx"
 
