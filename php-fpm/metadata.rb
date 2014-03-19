e30bb91128ed2604bca175a8f8198aaa7e022787
Dimetrio <dimetrio@flysoft.ru>
2014-03-19 17:10:38 +0400

    fix

--- php-fpm/metadata.rb
+++ php-fpm/metadata.rb
@@ -1,14 +1,8 @@
-name "php-fpm"
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
-license          "Apache 2.0"
-description      "Installs/Configures php-fpm"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.6.4"
+name             "nginx-php-fpm"
+maintainer       "Shen DeShayne"
+maintainer_email "shennyg@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures Nginx & PHP-FPM"
+version          "0.1.0"
+depends          "nginx"
 
-depends "apt"
-depends "yum"
-
-%w{ debian ubuntu centos redhat fedora amazon }.each do |os|
-  supports os
-end
--- php-fpm/recipes/default.rb
+++ php-fpm/recipes/default.rb
@@ -1,129 +1,66 @@
 #
-# Author::  Seth Chisamore (<schisamo@opscode.com>)
-# Cookbook Name:: php-fpm
+# Cookbook Name:: nginx-php-fpm
 # Recipe:: default
-#
-# Copyright 2011, Opscode, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Author:: Shen DeShayne <shennyg@gmail.com>
 #
 
-service_provider = nil
-
-case node['platform']
-when 'ubuntu'
-  if node['platform_version'].to_f <= 10.04
-    # Configure Brian's PPA
-    # We'll install php5-fpm from the Brian's PPA backports
-    apt_repository "brianmercer-php" do
-      uri "http://ppa.launchpad.net/brianmercer/php/ubuntu"
-      distribution node['lsb']['codename']
-      components ["main"]
-      keyserver "keyserver.ubuntu.com"
-      key "8D0DC64F"
-      action :add
-    end
-    # FIXME: apt-get update didn't trigger in above
-    execute "apt-get update"
-  end
-
-  if node['platform_version'].to_f >= 13.10
-    service_provider = ::Chef::Provider::Service::Upstart
-  end
-when 'debian'
-  # Configure Dotdeb repos
-  # TODO: move this to it's own 'dotdeb' cookbook?
-  # http://www.dotdeb.org/instructions/
-  if node.platform_version.to_f >= 7.0
-    apt_repository "dotdeb" do
-      uri "http://packages.dotdeb.org"
-      distribution "stable"
-      components ['all']
-      key "http://www.dotdeb.org/dotdeb.gpg"
-      action :add
-    end
-  elsif node.platform_version.to_f >= 6.0
-    apt_repository "dotdeb" do
-      uri "http://packages.dotdeb.org"
-      distribution "squeeze"
-      components ['all']
-      key "http://www.dotdeb.org/dotdeb.gpg"
-      action :add
-    end
-  else
-    apt_repository "dotdeb" do
-      uri "http://packages.dotdeb.org"
-      distribution "oldstable"
-      components ['all']
-      key "http://www.dotdeb.org/dotdeb.gpg"
-      action :add
-    end
-    apt_repository "dotdeb-php53" do
-      uri "http://php53.dotdeb.org"
-      distribution "oldstable"
-      components ['all']
-      key "http://www.dotdeb.org/dotdeb.gpg"
-      action :add
-    end
-  end
-
-when 'amazon', 'fedora', 'centos', 'redhat'
-  unless platform?('centos', 'redhat') && node['platform_version'].to_f >= 6.4
-    yum_key 'RPM-GPG-KEY-remi' do
-      url 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi'
-    end
-
-    yum_repository 'remi' do
-      description 'Remi'
-      url 'http://rpms.famillecollet.com/enterprise/$releasever/remi/$basearch/'
-      mirrorlist 'http://rpms.famillecollet.com/enterprise/$releasever/remi/mirror'
-      key 'RPM-GPG-KEY-remi'
-      action :add
-    end
-  end
+# create new user
+user_account node['user']['new_user'] do
+  ssh_keys node['user']['authorized_keys']
 end
 
-if platform_family?("rhel")
-  php_fpm_service_name = "php-fpm"
-else
-  php_fpm_service_name = "php5-fpm"
+# add user to www-data group
+group "www-data" do
+  action :modify
+  members node['user']['new_user']
+  append true
 end
 
-package php_fpm_service_name do
-  action :upgrade
+# install nginx
+include_recipe 'nginx'
+
+# create webroot
+directory node['nginx']['webroot'] do
+  recursive true
+  owner node['user']['new_user']
+  group "www-data"
+  mode 00755
+  action :create
 end
 
-template node['php-fpm']['conf_file'] do
-  source "php-fpm.conf.erb"
-  mode 00644
+# create php test file in the webroot
+template "#{node['nginx']['webroot']}/index.php" do
+  source "php-test.erb"
+  owner node['user']['new_user']
+  group "www-data"
+  mode 00755
+  only_if { node['show_phpinfo_as_index'] }
+end
+
+package "php5-mysql" do
+  action :install
+end
+
+package "php-apc" do
+  action :install
+end
+
+package "php5-curl" do
+  action :install
+  notifies :reload, 'service[php5-fpm]'
+end
+
+# create nginx server block file
+template "#{node['nginx']['dir']}/sites-available/webapp" do
+  source "webapp.erb"
   owner "root"
   group "root"
-  notifies :restart, "service[php-fpm]"
+  mode 00755
 end
 
-service "php-fpm" do
-  provider service_provider if service_provider
-  service_name php_fpm_service_name
-  supports :start => true, :stop => true, :restart => true, :reload => true
-  action [ :enable, :start ]
+# enable the server block we just created
+nginx_site 'webapp' do
+  enable true
+  notifies :reload, 'service[nginx]'
 end
 
-if node['php-fpm']['pools']
-  node['php-fpm']['pools'].each do |pool|
-    php_fpm_pool pool[:name] do
-      pool.each do |k, v|
-        self.params[k.to_sym] = v
-      end
-    end
-  end
-end
