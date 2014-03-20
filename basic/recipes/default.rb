15d739aafde254631df15f86ed4eb91d91eea0e4
Dimetrio <dimetrio@flysoft.ru>
2014-03-20 10:27:09 +0400

    test

--- basic/recipes/default.rb
+++ basic/recipes/default.rb
@@ -4,7 +4,23 @@
 # Author:: Dimetrio <kiddo.desu@gmail.com>
 #
 
+
+include_recipe 'php'
 # create new user
 user "ifunny" do
   username "ifunny"
+end
+
+
+php_fpm 'test' do
+  action :add
+  user 'ifunny'
+  group 'ifunny'
+  socket true
+  socket_path '/tmp/phpmyadmin.sock'
+  socket_perms "0666"
+  start_servers 2
+  min_spare_servers 2
+  max_spare_servers 8
+  max_children 8
 end
